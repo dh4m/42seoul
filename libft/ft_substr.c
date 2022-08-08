6988338f2093cc61c6552e90d3aa99341a40adf0
dh4m <dh4m28@gmail.com>
2022-08-08 15:13:14 +0900

    fix bug with overflow

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 16:29:06 by dham              #+#    #+#             */
-/*   Updated: 2022/07/06 14:51:01 by dham             ###   ########.fr       */
+/*   Updated: 2022/08/08 14:59:40 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,26 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*re_val;
+	char			*re_val;
+	const size_t	s_len = ft_strlen(s);
 
 	if (!s)
 		return (0);
-	if (ft_strlen(s) < start)
+	if (s_len < start)
 	{
 		re_val = malloc(1);
 		*re_val = 0;
 		return (re_val);
 	}
-	if (ft_strlen(s) - start > len)
+	if (s_len - start > len)
 		re_val = malloc(len + 1);
 	else
-		re_val = malloc(ft_strlen(s) - start + 1);
+		re_val = malloc(s_len - start + 1);
 	if (!re_val)
 		return (0);
-	ft_strlcpy(re_val, &s[start], len + 1);
+	if (s_len - start > len)
+		ft_strlcpy(re_val, &s[start], len + 1);
+	else
+		ft_strlcpy(re_val, &s[start], s_len - start + 1);
 	return (re_val);
 }
