3e4a956ae2baef294431bd2a3dbe7512a58d8e20
dh4m <dh4m28@gmail.com>
2022-08-15 16:46:15 +0900

    code improvement

--- philosophers/philo/take_fork.c
+++ philosophers/philo/take_fork.c
@@ -6,14 +6,14 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 13:29:56 by dham              #+#    #+#             */
-/*   Updated: 2022/08/14 19:53:43 by dham             ###   ########.fr       */
+/*   Updated: 2022/08/15 16:41:29 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "philo.h"
 
-int	take_fork(t_fork *fork)
+int	take_fork(t_philo *philo, t_fork *fork)
 {
 	while (1)
 	{
@@ -21,6 +21,8 @@ int	take_fork(t_fork *fork)
 		if (fork->state)
 		{
 			fork->state = 0;
+			printf("%d %d has taken a fork\n", \
+					diff_time(philo->info->s_time), philo->num);
 			pthread_mutex_unlock(&(fork->mutex));
 			return (1);
 		}
@@ -32,23 +34,15 @@ int	take_fork(t_fork *fork)
 
 int	take_leftfirst(t_philo *philo)
 {
-	take_fork(philo->left_fork);
-	printf("%d %d has taken a fork\n", \
-			diff_time(philo->info->s_time), philo->num);
-	take_fork(philo->right_fork);
-	printf("%d %d has taken a fork\n", \
-			diff_time(philo->info->s_time), philo->num);
+	take_fork(philo, philo->left_fork);
+	take_fork(philo, philo->right_fork);
 	return (0);
 }
 
 int	take_rightfirst(t_philo *philo)
 {
-	take_fork(philo->right_fork);
-	printf("%d %d has taken a fork\n", \
-			diff_time(philo->info->s_time), philo->num);
-	take_fork(philo->left_fork);
-	printf("%d %d has taken a fork\n", \
-			diff_time(philo->info->s_time), philo->num);
+	take_fork(philo, philo->right_fork);
+	take_fork(philo, philo->left_fork);
 	return (0);
 }
 
