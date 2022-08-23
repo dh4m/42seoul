bb4115b6a824908db6b98d5e321cacc0ac7a86a4
Dongkyun Ham <dh4m28@gmail.com>
2022-08-23 13:03:58 +0900

    print semaphore improvement

--- philosophers/philo_bonus/take_fork.c
+++ philosophers/philo_bonus/take_fork.c
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 15:42:25 by dham              #+#    #+#             */
-/*   Updated: 2022/08/23 12:46:58 by dham             ###   ########.fr       */
+/*   Updated: 2022/08/23 13:03:06 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ int	take_fork(t_info *info, sem_t *fork_sem)
 {
 	if (info->num % 2 == 0 && info->time_eat == 0)
 	{
-		printf("%d %d has taken a fork\n%d %d has taken a fork\n", \
-				diff_time(info->s_time), info->num, \
-				diff_time(info->s_time), info->num);
+		sem_print("%d %d has taken a fork\n", info->num, info);
+		sem_print("%d %d has taken a fork\n", info->num, info);
 		return (0);
 	}
 	sem_wait(fork_sem);
