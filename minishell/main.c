e80f9dfa35af41859f7f92fed5ed04cf2bb08914
Dongkyun Ham <dh4m28@gmail.com>
2022-08-27 16:50:03 +0900

    minishell exercise

---
+++ minishell/main.c
@@ -0,0 +1,22 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *str;
+
+	while (1)
+	{
+		str = readline("prompt : ");
+		if (str)
+			printf("%s\n", str);
+		else
+			break;
+		add_history(str);
+		free(str);
+	}
+	return (0);
+}
