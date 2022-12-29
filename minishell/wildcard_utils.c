e3b7dc3ac644db7011e63745876823216d683d7a
dham <dham@student.42seoul.kr>
2022-12-29 20:50:07 +0900

    minishell

---
+++ minishell/wildcard_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 16:52:31 by dham              #+#    #+#             */
+/*   Updated: 2022/12/29 18:56:26 by dham             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include "libft.h"
+#include "builtin/ft_builtin.h"
+#include "signal/ft_signal.h"
+#include "minishell.h"
+#include <dirent.h>
+
+char	**cur_dir_list(void)
+{
+	DIR				*dp;
+	struct dirent	*entry;
+	char			*ret_str;
+	
+	dp = opendir(".");
+}
