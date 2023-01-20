22f7adfefff9acfbabe92b3073064e9c05c35d3d
함동균 <dh4m28@gmail.com>
2023-01-21 00:35:31 +0900

    minishell

--- minishell/main.c
+++ minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 20:59:02 by dham              #+#    #+#             */
-/*   Updated: 2023/01/20 18:33:49 by dham             ###   ########.fr       */
+/*   Updated: 2023/01/20 23:17:29 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,7 @@ int	main(int argc, char *argv[], char *envp[])
 	while (1)
 	{
 		prompt_color(g_info.ret_val);
-		str = readline("minishell$ ");
-		write(1, "\033[m", 3);
+		str = readline("minishell$ \001\033[m\002");
 		//str = readline("minishell$ ");
 		if (str && !is_space_str(str) && *str != 0)
 		{
