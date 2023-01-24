e37d0f0d86dc4248aaa81562f608d0782f140feb
함동균 <dh4m28@gmail.com>
2023-01-24 19:15:53 +0900

    cpp module 01 ex05

--- CPP_Module_01/ex05/Harl.cpp
+++ CPP_Module_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 01:05:13 by dham              #+#    #+#             */
-/*   Updated: 2023/01/11 01:31:30 by dham             ###   ########.fr       */
+/*   Updated: 2023/01/24 19:15:30 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	Harl::complain(std::string level)
 		if (level.compare(level_list[i]) == 0)
 		{
 			(this->*m_func[i])();
+			return ;
 		}
 	}
 	std::cout << "Invalid input!" << '\n';
@@ -32,20 +33,22 @@ void	Harl::complain(std::string level)
 
 void	Harl::debug(void)
 {
-	;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n";
 }
 
 void	Harl::info(void)
 {
-	;
+	std::cout << "I cannot believe adding extra bacon costs more money. ";
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void	Harl::warning(void)
 {
-	;
+	std::cout << "I think I deserve to have some extra bacon for free. ";
+	std::cout << "I’ve been coming for years whereas you started working here since last month.\n";
 }
 
 void	Harl::error(void)
 {
-	;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
---
+++ CPP_Module_01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 18:43:05 by dham              #+#    #+#             */
+/*   Updated: 2023/01/24 19:14:54 by dham             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("INVALID");
+}
