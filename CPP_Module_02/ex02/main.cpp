a7ff0a4fb56a1428f254df30fcf20f12c71f52d9
함동균 <dh4m28@gmail.com>
2023-01-29 13:42:17 +0900

    cpp module 02 ex02

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,29 +6,29 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 00:18:37 by dham              #+#    #+#             */
-/*   Updated: 2023/01/29 12:59:35 by dham             ###   ########.fr       */
+/*   Updated: 2023/01/29 13:22:35 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-왜 b에서 생성자 메시지 안 나오는 지 알아볼것것;;
+//왜 b에서 생성자 메시지 안 나오는 지 알아볼것것;;
 
 int main( void ) {
 
 	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
+	Fixed const b= Fixed( 5.05f ) * Fixed( 2 ) ;
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	
+
 	std::cout << b << std::endl;
-	
+
 	std::cout << Fixed::max( a, b ) << std::endl;
-	
+
 	return 0;
 }
