60a4acf2eb34de245d1c45ce200df1da335aae5a
dham <dham@student.42seoul.kr>
2023-01-30 16:05:26 +0900

    CPP module03 ex00

---
+++ CPP_Module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 12:28:37 by dham              #+#    #+#             */
+/*   Updated: 2023/01/30 15:40:36 by dham             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class ClapTrap
+{
+public:
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &c);
+	~ClapTrap(void);
+
+	ClapTrap	&operator==(const ClapTrap &c);
+
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+private:
+	bool	able_action(void);
+	
+	std::string		_name;
+	unsigned int	_hit_point;
+	unsigned int	_energy_point;
+	unsigned int	_attack_damage;
+};
