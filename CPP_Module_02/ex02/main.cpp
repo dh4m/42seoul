69debac2f2b7750fd064112c19feac9ded31b166
dham <dham@student.42seoul.kr>
2023-02-01 11:58:46 +0900

    CPP module02 ex02

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,19 +6,17 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 00:18:37 by dham              #+#    #+#             */
-/*   Updated: 2023/01/29 13:43:16 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/01 11:58:18 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-왜 b에서 생성자 메시지 안 나오는 지 알아볼것것;;
-
 int main( void ) {
 
 	Fixed a;
-	Fixed const b= Fixed( 5.05f ) * Fixed( 2 ) ;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
