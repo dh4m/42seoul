84eb5623ff95ffb4af8693774bfe5725d31b75a1
함동균 <dh4m28@gmail.com>
2023-02-13 02:15:11 +0900

    cpp module 07

--- CPP_Module_07/ex02/Array.hpp
+++ CPP_Module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:19:12 by dham              #+#    #+#             */
-/*   Updated: 2023/02/13 01:21:06 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/13 02:07:53 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,73 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <iostream>
+
 template<typename T>
 class Array
 {
 public:
 	Array(void)
+	: _arr(NULL), _len(0)
+	{}
+
+	Array(unsigned int n)
+	: _len(n)
 	{
-		
+		_arr = new T[n];
 	}
 
-	Array(const Array &copy);
-	~Array(void);
+	Array(const Array &copy)
+	: _len(copy._len)
+	{
+		if (_len == 0)
+			_arr = NULL;
+		else
+		{
+			_arr = new T[_len];
+			for (unsigned int i = 0; i < _len; i++)
+			{
+				_arr[i] = copy._arr[i];
+			}
+		}
+	}
 
-	Array	&operator=(const Array &copy);
+	~Array(void)
+	{
+		delete[] _arr;
+	}
+
+	Array	&operator=(const Array &copy)
+	{
+		if (_arr)
+			delete[] _arr;
+		_len = copy._len;
+		if (_len == 0)
+			_arr = NULL;
+		else
+		{
+			_arr = new T[_len];
+			for (unsigned int i = 0; i < _len; i++)
+			{
+				_arr[i] = copy._arr[i];
+			}
+		}
+	}
+
+	T &operator[](int i)
+	{
+		if (_len == 0 || i >= _len || i < 0)
+			throw (std::exception());
+		return (_arr[i]);
+	}
+
+	unsigned int size(void) const
+	{
+		return (_len);
+	}
 private:
-
+	T *_arr;
+	unsigned int _len;
 };
 
 #endif
--- CPP_Module_07/ex02/main.cpp
+++ CPP_Module_07/ex02/main.cpp
@@ -6,7 +6,83 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:19:51 by dham              #+#    #+#             */
-/*   Updated: 2023/02/13 01:20:16 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/13 02:09:26 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+#include <iostream>
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+	// default value test
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << numbers[i] << " ";
+	}
+	std::cout << std::endl;
+    int* mirror = new int[MAX_VAL];
+	// value assignment test
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand() % 100;
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    // deep copy test and subscript operator test
+    {
+        Array<int> tmp = numbers;
+		for (int i = 0; i < 10; i++)
+		{
+			tmp[i] = numbers[i] + 1;
+		}
+        Array<int> test(tmp);
+		for (int i = 0; i < 10; i++)
+		{
+			test[i] = tmp[i] + 1;
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << i << " " << numbers[i] << " " << tmp[i] << " " << test[i] << std::endl;
+		}
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+	// out of range test
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+	// size member function test
+	std::cout << "numbers size: " << numbers.size() << std::endl;
+
+    delete [] mirror;//
+    return 0;
+}
