808ebdc488a4abf4e4e190e07cf6768a46902575
dham <dham@student.42seoul.kr>
2023-02-12 18:16:00 +0900

    CPP module06 ex03

--- CPP_Module_06/ex02/main.cpp
+++ CPP_Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 16:52:50 by dham              #+#    #+#             */
-/*   Updated: 2023/02/12 16:59:46 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/12 18:12:26 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int main(void)
 	base_ptr = generate();
 	identify(base_ptr);
 	identify(obj);
+	delete base_ptr;
 	return (0);
 }
