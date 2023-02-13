4c6a74a1a0054b63ce0a73b2f5cf67ee02e8136d
dham <dham@student.42seoul.kr>
2023-02-13 14:30:25 +0900

    CPP module07 ex01

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,29 +6,36 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 19:50:14 by dham              #+#    #+#             */
-/*   Updated: 2023/02/13 01:18:15 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/13 14:21:43 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
 
-
-void print_int(int i)
+template<typename T>
+void print_int(const T i)
 {
 	std::cout << i << std::endl;
 }
 
-void increament_int(int &i)
+template<typename T>
+void increament(T &i)
 {
 	i++;
 }
 
+template<>
+void increament<std::string>(std::string &i)
+{
+	i += "_test";
+}
+
 int main(void)
 {
-	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::string arr[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-	iter(arr, 10, print_int);
-	iter(arr, 10, increament_int);
-	iter(arr, 10, print_int);
+	iter(arr, 10, print_int<std::string>);
+	iter(arr, 10, increament<std::string>);
+	iter(arr, 10, print_int<std::string>);
 }
