e345553419822be2e46942fb5360ad967e127480
함동균 <dh4m28@gmail.com>
2023-02-26 23:10:32 +0900

    minirt

--- miniRT/main.c
+++ miniRT/main.c
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:19:03 by dham              #+#    #+#             */
-/*   Updated: 2023/02/21 17:14:42 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/22 19:28:40 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 
 int	main(int argc, char *argv[])
 {
-	t_info info;
+	t_info	info;
+	t_img	img;
 
 	if (argc != 2)
 		invalid_args();
 	info.mlx = mlx_init();
-	info.win = mlx_new_window(info.mlx, SCREEN_SIZE, SCREEN_SIZE, "miniRT");
-	
-
+	info.win = mlx_new_window(info.mlx, WIDTH, HEIGHT, "miniRT");
+	img.img = mlx_new_image(info.mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+									&img.line_length, &img.endian);
+	///calculate
+	mlx_put_image_to_window(info.mlx, info.win, img.img, 0, 0);
 	mlx_hook(info.win, 17, 0, win_destroy, 0);
 	mlx_loop(info.mlx);
 	return (0);
--- miniRT/minirt.h
+++ miniRT/minirt.h
@@ -6,33 +6,82 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:23:00 by dham              #+#    #+#             */
-/*   Updated: 2023/02/21 17:14:38 by dham             ###   ########.fr       */
+/*   Updated: 2023/02/22 22:38:50 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# define SCREEN_SIZE 800
+# define WIDTH 1200
+# define HEIGHT 800
 
-enum asdf/////
+enum e_return_value
+{
+	SUCCESS,
+	RT_FILE_OPEN_ERROR = -1,
+	UNAVILABLE_RT_FILE = -2,
+	TEXTURE_FILE_OPEN_ERROR = -3,
+	UNAVILABLE_RT_FILE = -4,
+	FATAL_ERROR = -5
+};
+
+enum e_shape
+{
+	SPHERE,
+	PLANE,
+	CYLINDER,
+	CONE
+};
+
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
 
 typedef struct s_vec
 {
-	double x;
-	double y;
-	double z;
+	float	x;
+	float	y;
+	float	z;
 }	t_vec;
 
 typedef struct s_ray
 {
-	t_vec start;
-	t_vec dir;
-}	t_rey;
+	t_vec	start;
+	t_vec	dir;
+}	t_ray;
+
+typedef struct s_camera
+{
+	t_vec	loc;
+	t_vec	dir;
+	int		fov;
+}	t_camera;
+
+typedef struct s_light
+{
+	int				ambient;
+	t_vec			loc;
+	t_color			color;
+	float			bright;
+	struct s_light	*next;
+}	t_light;
 
 typedef struct s_obj
 {
-	t_vec 
+	int				shape;
+	t_vec			loc;
+	float			diameter;
+	float			height;
+	float			theta;
+	t_color			color;
+	t_vec			nomal_v;
+	//				texture
+	//				bump map texture
+	struct s_obj	*next;
 }	t_obj;
 
 typedef struct s_info
@@ -54,5 +103,7 @@ void	invalid_args(void);
 
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color);
 int		win_destroy(void *param);
+
+int		make_image(t_info *info, t_img *img, char *rt_file);
 
 #endif
