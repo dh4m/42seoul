243477a7c8eac9b60221472a2fa01b40753a3025
dham <dham@student.42seoul.kr>
2023-06-27 21:04:14 +0900

    irc

--- ft_irc/Eventq.cpp
+++ ft_irc/Eventq.cpp
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:05:07 by dham              #+#    #+#             */
-/*   Updated: 2023/06/27 17:17:34 by dham             ###   ########.fr       */
+/*   Updated: 2023/06/27 21:03:56 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,7 @@ int Eventq::init(void)
 int Eventq::reg_event(int socket, int16_t filter, uint16_t flag, uint16_t fflage, intptr_t data, void *udata)
 {
 	t_event temp;
-	// https://opensource.apple.com/source/xnu/xnu-4570.41.2/bsd/kern/kern_event.c.auto.html
-	// https://github.com/libevent/libevent/blob/master/evthread-internal.h
-	// https://github.com/libevent/libevent/blob/master/evthread.c
-	// https://libevent.org/doc/structevthread__lock__callbacks.html
+
 	EV_SET(&temp, socket, filter, flag, fflage, data, udata);
 	kevent(_kq, &temp, 1, NULL, 0, NULL);
 	return (1);
