3993bf3c7655b4c5a048dffcbc2a8d2f7e9df5de
dham <dham@student.42seoul.kr>
2023-07-05 21:24:34 +0900

    irc

--- ft_irc/Eventq.cpp
+++ ft_irc/Eventq.cpp
@@ -6,12 +6,13 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:05:07 by dham              #+#    #+#             */
-/*   Updated: 2023/06/28 21:49:35 by dham             ###   ########.fr       */
+/*   Updated: 2023/07/05 19:09:25 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Eventq.hpp"
 #include <iostream>
+#include <fcntl.h>
 
 Eventq::Eventq(void) {}
 Eventq::~Eventq(void) {}
@@ -35,6 +36,9 @@ int Eventq::init(void)
 		;/// err
 		return (0);
 	}
+	fcntl(_trigger_pipe[0], F_SETFL, O_NONBLOCK);
+	fcntl(_trigger_pipe[1], F_SETFL, O_NONBLOCK);
+	pthread_mutex_init(&_mutex_change_list, NULL);
 	reg_event(_trigger_pipe[0], EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 	return (1);
 }
@@ -44,14 +48,40 @@ int Eventq::reg_event(int socket, int16_t filter, uint16_t flag, uint16_t fflage
 	t_event temp;
 
 	EV_SET(&temp, socket, filter, flag, fflage, data, udata);
-	kevent(_kq, &temp, 1, NULL, 0, NULL);
+	pthread_mutex_lock(&_mutex_change_list);
+	_change_list.push_back(temp);
+	_trigger_send();
+	pthread_mutex_unlock(&_mutex_change_list);
 	return (1);
 }
 
 int Eventq::get_event(t_event event[], int len)
 {
 	int event_num;
-
+	std::vector<t_event> tmp_list;
+	
 	event_num = kevent(_kq, NULL, 0, event, len, NULL);
+
+	do
+	{
+		pthread_mutex_lock(&_mutex_change_list);
+		if (_change_list.size())
+			kevent(_kq, _change_list.data(), _change_list.size(), NULL, 0, NULL);
+		read(_trigger_pipe[0], _flush_buf, PIPE_BUF);
+		pthread_mutex_unlock(&_mutex_change_list);
+		event_num = kevent(_kq, NULL, 0, event, len, NULL);
+		
+	} while (event_num == 1 && event[0].ident == _trigger_pipe[0]);
 	return (event_num);
+}
+
+int Eventq::_trigger_send(void)
+{
+	write(_trigger_pipe[1], "\0", 1);
+}
+
+void Eventq::destroy_eventq(void)
+{
+	close(_trigger_pipe[0]);
+	close(_trigger_pipe[1]);
 }
--- ft_irc/include/Eventq.hpp
+++ ft_irc/include/Eventq.hpp
@@ -6,7 +6,7 @@
 /*   By: dham <dham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:03:05 by dham              #+#    #+#             */
-/*   Updated: 2023/06/28 13:10:10 by dham             ###   ########.fr       */
+/*   Updated: 2023/07/05 21:23:32 by dham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@
 #include <vector>
 #include <pthread.h>
 
+#ifndef PIPE_BUF
+# define PIPE_BUF 512
+#endif
+
 typedef struct kevent t_event;
 
 class Eventq
@@ -32,12 +36,15 @@ public:
 	int init(void);
 	int reg_event(int socket, int16_t filter, uint16_t flag, uint16_t fflage, intptr_t data, void *udata);
 	int get_event(t_event event[], int len);
-	int trigger_send(void);
-	// void destroy_eventq(void);
+	void destroy_eventq(void);
 private:
 	int _kq;
 	int _trigger_pipe[2];
-	std::vector<t_event> change_list;
+	std::vector<t_event> _change_list;
+	pthread_mutex_t _mutex_change_list;
+	char _flush_buf[PIPE_BUF];
+
+	int _trigger_send(void);
 	
 	Eventq	&operator=(const Eventq &copy);
 	Eventq(const Eventq &copy);
